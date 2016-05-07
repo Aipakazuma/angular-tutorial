7978a18059f4bee304dc21db7bc322238fa2855f
aipacommander <gundamgundam777@gmail.com>
2016-05-08 07:20:07 +0900

    first commit

---
+++ controller.js
@@ -0,0 +1,28 @@
+var app = angular.module('App', []);
+
+app.controller('AppController', function($scope){
+  $scope.display = function(){
+    var str = $scope.displayText;
+    $scope.text = str;
+  }
+  $scope.comments = [
+      {name: "Yamada", text: "Hello!"},
+      {name: "Tanaka", text: "Thanks!"},
+      {name: "Nakamura", text: "Yeah!"}
+  ];
+});
+
+app.controller('mainCtrl', function($scope) {
+  $scope.users = [
+    {'name':'yamada', 'score':'1000'},
+    {'name':'okinawa', 'score':'1001'},
+    {'name':'okinawa1', 'score':'1001'},
+    {'name':'okinawa2', 'score':'1001'},
+    {'name':'okinawa3', 'score':'1001'},
+    {'name':'okinawa4', 'score':'1201'},
+    {'name':'okinawa5', 'score':'1001'},
+    {'name':'okinawa6', 'score':'1001'},
+    {'name':'okinawa7', 'score':'1001'},
+  ];
+  $scope.today = new Date();
+});
